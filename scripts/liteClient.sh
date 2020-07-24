8c8e33806041a6eaef70ce43844616a6068269fc
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-07-24 22:24:35 +0000

    Update liteClient.sh

--- scripts/liteClient.sh
+++ scripts/liteClient.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cd ~/main.ton.dev/ton/build/lite-client && \
-./lite-client \
--p ~/ton-keys/liteserver.pub \
--a 127.0.0.1:3031 \
--rc \
-"${1} ${2} ${3} ${4}"
+${BUILD_DIR}/lite-client/lite-client \
+  -p ~/ton-keys/liteserver.pub \
+  -a 127.0.0.1:3031 \
+  -rc \
+  "${1} ${2} ${3} ${4}"
+  -rc 'quit' 2>/dev/null
