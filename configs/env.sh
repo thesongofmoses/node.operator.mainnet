41a27e96c31a01f702cf2b766bd7195ba06bc770
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-07-24 14:40:31 +0000

    Update env.sh

--- configs/env.sh
+++ configs/env.sh
@@ -5,30 +5,34 @@ then
     set -x
 fi
 
-SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)
-# Verified on Ubuntu 18.04
-export INSTALL_DEPENDENCIES="yes"
-#NET_TON_DEV_SRC_TOP_DIR=$(git rev-parse --show-toplevel)
-NET_TON_DEV_SRC_TOP_DIR=$(cd "${SCRIPT_DIR}/../" && pwd -P)
-export NET_TON_DEV_SRC_TOP_DIR
-export TON_SRC_DIR="${NET_TON_DEV_SRC_TOP_DIR}/ton"
-export TON_BUILD_DIR="${TON_SRC_DIR}/build"
-export TONOS_CLI_SRC_DIR="${NET_TON_DEV_SRC_TOP_DIR}/tonos-cli"
-export TON_WORK_DIR="/var/ton-work"
-export UTILS_DIR="${TON_BUILD_DIR}/utils"
-export KEYS_DIR="$HOME/ton-keys"
-export CONFIGS_DIR="${NET_TON_DEV_SRC_TOP_DIR}/configs"
-export ADNL_PORT="30310"
+export NETWORK="main"
 HOSTNAME=$(hostname -s)
 export HOSTNAME
-export VALIDATOR_NAME="$HOSTNAME"
-export PATH="${UTILS_DIR}:$PATH"
-export LITESERVER_IP="127.0.0.1"
-export LITESERVER_PORT="3031"
-export ENGINE_ADDITIONAL_PARAMS=""
-
 USERNAME=$(whoami)
 export USERNAME
+
+#DIRs
+##x.ton.dev
+export DEV_DIR="${NETWORK}.ton.dev"
+export TON_DIR="${DEV_DIR}/ton"
+export BUILD_DIR="${TON_DIR}/build"
+export SCRIPTS_DIR="${DEV_DIR}/scripts"
+export UTILS_DIR="${TON_BUILD_DIR}/utils"
+export CONFIGS_DIR="${DEV_DIR}/configs"
+
+##ton-keys
+export KEYS_DIR="$HOME/ton-keys"
+export ELECTIONS_DIR="${KEYS_DIR}/elections"
+
+##ton-work
+export TON_WORK_DIR="/var/ton-work"
+export TON_WORK_DB_DIR="${TON_WORK_DIR}/db"
+export TON_WORK_DB_KEYRING_DIR="${TON_WORK_DB_DIR}/keyring"
+
+##tonos-cli
+export TONOS_CLI_DIR="${DEV_DIR}/tonos-cli/target/release"
+
+##node.operator
 export NODE_OPERATOR_DIR="$HOME/node.operator"
 export NODE_OPERATOR_SCRIPTS_DIR="${NODE_OPERATOR_DIR}/scripts"
-
+export NODE_OPERATOR_CONFIGS_DIR="${NODE_OPERATOR_DIR}/configs"
