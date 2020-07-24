8d32b1b570c78bf150f2c3c260eb569939ae24ba
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-07-24 21:40:25 +0000

    Update fift.sh

--- scripts/fift.sh
+++ scripts/fift.sh
@@ -4,5 +4,5 @@ NODE_OPERATOR_CONFIGS_DIR="~/node.operator/configs"
 . "${NODE_OPERATOR_CONFIGS_DIR}/env.sh"
 
 cd ${BUILD_DIR}
-crypto/fift -I/home/$USERNAME/${TON_DIR}/crypto/fift/lib \
+crypto/fift -I/${TON_DIR}/crypto/fift/lib \
 -s ${TON_DIR}/crypto/smartcont/${1}
