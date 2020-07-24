d4db1ced1c6894dae5e088d5b2f158b5d3d7673b
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-07-24 15:29:15 +0000

    Update env.sh

--- configs/env.sh
+++ configs/env.sh
@@ -18,7 +18,7 @@ export SAFEMULTISIGWALLET_TVC="${SAFEMULTISIG_DIR}/SafeMultisigWallet.tvc"
 
 #DIRs
 ##x.ton.dev
-export DEV_DIR="${NETWORK}.ton.dev"
+export DEV_DIR="$HOME/${NETWORK}.ton.dev"
 export TON_DIR="${DEV_DIR}/ton"
 export BUILD_DIR="${TON_DIR}/build"
 export SCRIPTS_DIR="${DEV_DIR}/scripts"
@@ -29,15 +29,15 @@ export CONFIGS_DIR="${DEV_DIR}/configs"
 export KEYS_DIR="$HOME/ton-keys"
 export ELECTIONS_DIR="${KEYS_DIR}/elections"
 
+##smart-contracts
+export SOLIDITY_DIR="$HOME/${DEV_DIR}/ton-labs-contracts/solidity"
+export SETCODEMULTISIG_DIR="${SOLIDITY_DIR}/setcodemultisig"
+export SAFEMULTISIG_DIR="${SOLIDITY_DIR}/safemultisig"
+
 ##ton-work
 export TON_WORK_DIR="/var/ton-work"
 export TON_WORK_DB_DIR="${TON_WORK_DIR}/db"
 export TON_WORK_DB_KEYRING_DIR="${TON_WORK_DB_DIR}/keyring"
-
-##smart-contracts
-export SOLIDITY_DIR="${DEV_DIR}/ton-labs-contracts/solidity"
-export SETCODEMULTISIG_DIR="${SOLIDITY_DIR}/setcodemultisig"
-export SAFEMULTISIG_DIR="${SOLIDITY_DIR}/safemultisig"
 
 #TONOS-CLI
 export TONOS_CLI_DIR="${DEV_DIR}/tonos-cli/target/release"
