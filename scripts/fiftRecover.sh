ca8c2183e1549cd82d64826e43c9c40ec78e5013
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-07-25 00:38:41 +0000

    Update fiftRecover.sh

--- scripts/fiftRecover.sh
+++ scripts/fiftRecover.sh
@@ -9,8 +9,8 @@ my_parsed_raw_address=$(echo $MY_RAW_ADDRESS | sed 's/^.\{3\}//')
 touch $my_parsed_raw_address > ${TVM_DIR}/$my_parsed_raw_address
 
 #prepare key pair
-cat ~/ton-keys/msig* | awk 'FNR == 3 {print $2}' | tr -d '"\,\n' > ${TVM_DIR}/msig.keys.txt
-cat ~/ton-keys/msig* | awk 'FNR == 2 {print $2}' | tr -d '"\,\n' >> ${TVM_DIR}/msig.keys.txt
+cat ${KEYS_DIR}/msig.keys.json | awk 'FNR == 3 {print $2}' | tr -d '"\,\n' > ${TVM_DIR}/msig.keys.txt
+cat ${KEYS_DIR}/msig.keys.json | awk 'FNR == 2 {print $2}' | tr -d '"\,\n' >> ${TVM_DIR}/msig.keys.txt
 xxd -r -p ${TVM_DIR}/msig.keys.txt ${TVM_DIR}/msig.keys.bin
 
 #prepare message
