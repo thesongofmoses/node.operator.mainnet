e4a8a3bb67e36f63afcd5a66b9d6bc7ca9bf66c6
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-07-25 01:20:28 +0000

    Update validatorBuild.sh

--- scripts/validatorBuild.sh
+++ scripts/validatorBuild.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-sudo apt install -y git
-cd && git clone https://github.com/tonlabs/${NETWORK}.ton.dev.git
+sudo apt update -y
+sudo apt upgrade -y
+
+NODE_OPERATOR_SCRIPTS_DIR='$HOME/node.operator/scripts'
+NODE_OPERATOR_CONFIGS_DIR='$HOME/node.operator/configs'
+
+#install dependencies
+sudo apt install bc -y
+mv node.operator.mainnet node.operator
+cd ${NODE_OPERATOR_SCRIPTS_DIR} && . ${NODE_OPERATOR_CONFIGS_DIR}/env.sh ${1}
+chmod +x -R ${NODE_OPERATOR_SCRIPTS_DIR}
+cd ${NODE_OPERATOR_CONFIGS_DIR}
+    cat bashrc > ~/.bashrc && source ~/.bashrc
+    crontab ~/node.operator/configs/crontab
+
+#install node
+cd && git clone https://github.com/tonlabs/${1}.ton.dev.git
 cd ${SCRIPTS_DIR}
 . ./env.sh
 ./build.sh
