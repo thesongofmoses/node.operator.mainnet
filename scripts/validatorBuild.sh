5b0de2e52e0923ca24ce8ab72e4a812f3c8b72e9
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-07-25 00:58:01 +0000

    defined scripts_dir

--- scripts/validatorBuild.sh
+++ scripts/validatorBuild.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+SCRIPTS_DIR='$HOME/main.ton.dev/scripts'
+
 sudo apt install -y git
+cd
 git clone https://github.com/tonlabs/${NETWORK}.ton.dev.git
+
 cd ${SCRIPTS_DIR}
 . ./env.sh
 ./build.sh
