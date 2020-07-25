5ff15db86fa67a0488669abd4d0d0c7d8245a789
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-07-25 00:59:12 +0000

    Update validatorBuild.sh

--- scripts/validatorBuild.sh
+++ scripts/validatorBuild.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-SCRIPTS_DIR='$HOME/main.ton.dev/scripts'
-
 sudo apt install -y git
-cd
-git clone https://github.com/tonlabs/${NETWORK}.ton.dev.git
-
+cd && git clone https://github.com/tonlabs/${NETWORK}.ton.dev.git
 cd ${SCRIPTS_DIR}
 . ./env.sh
 ./build.sh
 ./setup.sh
 ./run.sh
 
-cd
-git clone https://github.com/tonlabs/TVM-linker.git
-cd TVM-linker/tvm_linker
+cd && git clone https://github.com/tonlabs/TVM-linker.git
+cd ${TVM_DIR}
 cargo build --release
