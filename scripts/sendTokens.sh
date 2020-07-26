56ad51464438f40ec35cfe0944969d08c8555748
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-07-26 10:42:58 +0000

    Update sendTokens.sh

--- scripts/sendTokens.sh
+++ scripts/sendTokens.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+check_status=$(cd ${NODE_OPERATOR_SCRIPTS_DIR} && \
+./checkAccount.sh ${1} | grep account | awk 'FNR == 3 {print $1}' | perl -ne '/state:\(account_(\S+)/ && print $1')
+
+
+if [ "$check_status" == 'active' ];
+then
 cd ${TONOS_CLI_DIR} && \
 ./tonos-cli call $MY_RAW_ADDRESS \
         submitTransaction "{"\"dest"\":"\"${1}"\","\"value"\":${2},"\"bounce"\":true,"\"allBalance"\":false,"\"payload"\":"\""\"}" \
         --abi "${SAFEMULTISIG_DIR}/SafeMultisigWallet.abi.json" \
         --sign ${KEYS_DIR}/msig.keys.json
+else
+cd ${TONOS_CLI_DIR} && \
+./tonos-cli call $MY_RAW_ADDRESS \
+        submitTransaction "{"\"dest"\":"\"${1}"\","\"value"\":${2},"\"bounce"\":true,"\"allBalance"\":true,"\"payload"\":"\""\"}" \
+        --abi "${SAFEMULTISIG_DIR}/SafeMultisigWallet.abi.json" \
+        --sign ${KEYS_DIR}/msig.keys.json
+fi
