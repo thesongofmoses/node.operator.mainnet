04bd84e0c7eeddd88a363a25c433688d39206a96
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-07-29 07:33:42 +0000

    Update sendTokens.sh

--- scripts/sendTokens.sh
+++ scripts/sendTokens.sh
@@ -1,44 +1,42 @@
 #!/bin/bash
 
-check_status_1=$(cd ${NODE_OPERATOR_SCRIPTS_DIR} && \
+check_status_one=$(cd ${NODE_OPERATOR_SCRIPTS_DIR} && \
 ./liteClient.sh getaccount ${1} | awk 'FNR == 19 {print $1}' | sed 's/^.\{15\}//')
 
-check_status_2=$(cd ${NODE_OPERATOR_SCRIPTS_DIR} && \
+check_status_two=$(cd ${NODE_OPERATOR_SCRIPTS_DIR} && \
 ./liteClient.sh getaccount ${2} | awk 'FNR == 19 {print $1}' | sed 's/^.\{15\}//')
 
-if [ "$check_status" == 'active' ];
+if [ "$check_status_one" == 'active' ] && [ $# == 2 ];
 then
-        if [ $# == 2 ];
-        then
-        cd ${TONOS_CLI_DIR} && \
-        ./tonos-cli call $MY_RAW_ADDRESS \
-                submitTransaction "{"\"dest"\":"\"${1}"\","\"value"\":${2},"\"bounce"\":true,"\"allBalance"\":false,"\"payload"\":"\""\"}" \
-                --abi "${SAFEMULTISIG_DIR}/SafeMultisigWallet.abi.json" \
-                --sign ${KEYS_DIR}/msig.keys.json
+cd ${TONOS_CLI_DIR} && \
+./tonos-cli call $MY_RAW_ADDRESS \
+        submitTransaction "{"\"dest"\":"\"${1}"\","\"value"\":${2},"\"bounce"\":true,"\"allBalance"\":false,"\"payload"\":"\""\"}" \
+        --abi "${SAFEMULTISIG_DIR}/SafeMultisigWallet.abi.json" \
+        --sign ${KEYS_DIR}/msig.keys.json
 
-        elif [ $# == 3 ];
-        then
-        cd ${TONOS_CLI_DIR} && \
-        ./tonos-cli call ${1:-MY_RAW_ADDRESS} \
-                submitTransaction "{"\"dest"\":"\"${2}"\","\"value"\":${3},"\"bounce"\":true,"\"allBalance"\":false,"\"payload"\":"\""\"}" \
-                --abi "${SAFEMULTISIG_DIR}/SafeMultisigWallet.abi.json" \
-                --sign ${KEYS_DIR}/msig.keys.json
-        fi
+elif [ "$check_status_two" == 'active' ] && [ $# == 4 ];
+then
+cd ${TONOS_CLI_DIR} && \
+./tonos-cli call ${1} \
+        submitTransaction "{"\"dest"\":"\"${2}"\","\"value"\":${3},"\"bounce"\":true,"\"allBalance"\":false,"\"payload"\":"\""\"}" \
+        --abi "${SAFEMULTISIG_DIR}/SafeMultisigWallet.abi.json" \
+        --sign ${KEYS_DIR}/${4}
+elif [ "$check_status_one" != 'active' ] && [ $# == 2 ];
+then
+cd ${TONOS_CLI_DIR} && \
+./tonos-cli call $MY_RAW_ADDRESS \
+        submitTransaction "{"\"dest"\":"\"${1}"\","\"value"\":${2},"\"bounce"\":false,"\"allBalance"\":false,"\"payload"\":"\""\"}" \
+        --abi "${SAFEMULTISIG_DIR}/SafeMultisigWallet.abi.json" \
+        --sign ${KEYS_DIR}/msig.keys.json
+
+elif [ "$check_status_two" != 'active' ] && [ $# == 4 ];
+then
+cd ${TONOS_CLI_DIR} && \
+./tonos-cli call ${1} \
+        submitTransaction "{"\"dest"\":"\"${2}"\","\"value"\":${3},"\"bounce"\":false,"\"allBalance"\":false,"\"payload"\":"\""\"}" \
+        --abi "${SAFEMULTISIG_DIR}/SafeMultisigWallet.abi.json" \
+        --sign ${KEYS_DIR}/${4}
 else
-        if [ $# == 2 ];
-        then
-        cd ${TONOS_CLI_DIR} && \
-        ./tonos-cli call $MY_RAW_ADDRESS \
-                submitTransaction "{"\"dest"\":"\"${1}"\","\"value"\":${2},"\"bounce"\":false,"\"allBalance"\":false,"\"payload"\":"\""\"}" \
-                --abi "${SAFEMULTISIG_DIR}/SafeMultisigWallet.abi.json" \
-                --sign ${KEYS_DIR}/msig.keys.json
+echo "ERROR"
 
-        elif [ $# == 3 ];
-        then
-        cd ${TONOS_CLI_DIR} && \
-        ./tonos-cli call ${1:-MY_RAW_ADDRESS} \
-                submitTransaction "{"\"dest"\":"\"${2}"\","\"value"\":${3},"\"bounce"\":false,"\"allBalance"\":false,"\"payload"\":"\""\"}" \
-                --abi "${SAFEMULTISIG_DIR}/SafeMultisigWallet.abi.json" \
-                --sign ${KEYS_DIR}/msig.keys.json
-        fi
 fi
