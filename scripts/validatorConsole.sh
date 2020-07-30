5a1a555f8a7737c06f3e479d7621ce26dd8d30c6
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-07-30 22:02:58 +0000

    Update validatorConsole.sh

--- scripts/validatorConsole.sh
+++ scripts/validatorConsole.sh
@@ -4,4 +4,4 @@ cd ${BUILD_DIR}
 validator-engine-console/validator-engine-console \
         -k ${KEYS_DIR}/client \
         -p ${KEYS_DIR}/server.pub \
-        -a 127.0.0.1:3030
+        -a ${LITESERVER_IP}:${LITESERVER_PORT}
