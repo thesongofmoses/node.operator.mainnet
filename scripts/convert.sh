abf3d201d7e64a169b335957baeae63ca6fe3d14
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-08-02 23:24:29 +0000

    Update convert.sh

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-permkey=$(cat ${TON_WORK_DB_DIR}/config.json | jq -r ".validators[0].id" | base64 -d | xxd -p -c 64)
-tempkey=$(cat ${TON_WORK_DB_DIR}/config.json | jq -r ".validators[0].temp_keys[0].key" | base64 -d | xxd -p -c 64)
-adnl_addrs=$(cat ${TON_WORK_DB_DIR}/config.json | jq -r ".validators[0].adnl_addrs[0].id" | base64 -d | xxd -p -c 64)
+permkey_hash=$(cat ${TON_WORK_DB_DIR}/config.json | jq -r ".validators[0].id" | base64 -d | xxd -p -c 64)
+tempkey_hash=$(cat ${TON_WORK_DB_DIR}/config.json | jq -r ".validators[0].temp_keys[0].key" | base64 -d | xxd -p -c 64)
+adnlkey_hash=$(cat ${TON_WORK_DB_DIR}/config.json | jq -r ".validators[0].adnl_addrs[0].id" | base64 -d | xxd -p -c 64)
 
-echo "permkey: $permkey"
-echo "tempkey: $tempkey"
-echo "adnlkey: $adnl_addrs"
+echo "permkey_hash: $permkey_hash"
+echo "tempkey_hash: $tempkey_hash"
+echo "adnlkey_hash: $adnlkey_hash"
