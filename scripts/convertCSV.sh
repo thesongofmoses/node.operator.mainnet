d44187c130fa5fced4c8e47b6fb59b5fcfa47b3f
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-08-05 03:01:02 +0000

    Update convertCSV.sh

--- scripts/convertCSV.sh
+++ scripts/convertCSV.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-input="${1}.csv"
-while IFS=',' read -r f1 f2 f3 f4 f5
-do
-        echo "$f1 $f2 $f3 $f4 $f5" > ${1}.txt
-done < "$input"
+rm "${1}.txt"
 
+input="${1}.csv"
+while IFS=',' read -r dest tokens
+do
+        echo "$dest $tokens" >> "${1}.txt"
+done < "$input"
