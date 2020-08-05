ca933b1f49aeea503a3c09bafe910e50b2953129
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-08-05 02:45:32 +0000

    Update and rename sendBatch.sh to convertCSV.sh

--- scripts/convertCSV.sh
+++ scripts/convertCSV.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-input="<X.csv>"
+input="${1}.csv"
 while IFS=',' read -r f1 f2 f3 f4 f5
 do
-        echo "$f1 $f2 $f3 $f4 $f5"
+        echo "$f1 $f2 $f3 $f4 $f5" > ${1}.txt
 done < "$input"
 
