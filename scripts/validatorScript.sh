409d9458e4d7d5fba6731943f152c85c9ac9eb06
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-08-11 07:04:02 +0000

    Create validatorScript.sh

---
+++ scripts/validatorScript.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+. ${NODE_OPERATOR_CONFIGS_DIR}/env.sh
+
+nano_balance=$(cd ${NODE_OPERATOR_SCRIPTS_DIR} && \
+        ./liteClient.sh "getaccount ${MY_RAW_ADDRESS}" | tail -n 1 | tr -d '=''ng' | awk '{print $4}')
+fees_reserve='1000000000'
+stake=$(echo $((($nano_balance-$fees_reserve)/1000000000)))
+
+
+sleep_by='600'
+sleep_delay='300'
+sleep="$(($RANDOM% $sleep_by+$sleep_delay))"
+
+check_election_status=$(cd ${NODE_OPERATOR_SCRIPTS_DIR} && \
+        ./liteClient.sh "runmethodfull ${ELECTOR_ADDRESS} active_election_id" | awk 'FNR == 5 {print $3}')
+
+if [ $check_election_status != 0 ];
+then
+        sleep $sleep && \
+        cd ${SCRIPTS_DIR} && \
+        ./validator_msig.sh $stake >> ${NODE_OPERATOR_LOGS_DIR}/validator.log
+fi
