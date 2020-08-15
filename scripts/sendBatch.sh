f5959473a553dccdeee6687408cecb6ffda83f4c
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-08-15 07:28:50 +0000

    Update sendBatch.sh

--- scripts/sendBatch.sh
+++ scripts/sendBatch.sh
@@ -15,15 +15,16 @@ do
         echo "$address $tokens" >> "${1}.txt"
 done < "$input"
 
+n=$(cat ${1}.csv | wc -l)
 rm "${1}_status.txt"
-for i in {1..100}
+for (( i = 1; i <= n; i++ ))
 do
         cd ${NODE_OPERATOR_SCRIPTS_DIR} && \
         ./liteClient.sh getaccount $(sed -n ${i}p "${NODE_OPERATOR_SCRIPTS_DIR}/${1}.txt" | awk '{print $1}') | awk 'FNR == 19 {print $1}' | sed 's/^.\{15\}//' | awk '{print} END {if (!NR) print "empty"}' >> "${1}_status.txt"
 done
 
 rm ${NODE_OPERATOR_SCRIPTS_DIR}/${1}_txid.txt
-for i in {1..100}
+for (( i = 1; i <= n; i++ ))
 do
 
 if [ "$(sed -n ${i}p ${NODE_OPERATOR_SCRIPTS_DIR}/${1}_status.txt)" == 'active' ];
