07d69219260cd4eb74e3d840b72b49b90213ad28
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-11-24 00:03:17 +0000

    Update validatorUpdate.sh

--- scripts/validatorUpdate.sh
+++ scripts/validatorUpdate.sh
@@ -8,8 +8,8 @@ git reset --hard origin/master
 cd ~/main.ton.dev/scripts && ./build.sh
 cd ~/main.ton.dev/ton
 git rev-parse HEAD
-head -n 2 /var/ton-work/node.log
 rm -f /var/ton-work/etc/ton-global.config.json
 cd /var/ton-work/etc/
 wget https://raw.githubusercontent.com/tonlabs/main.ton.dev/master/configs/ton-global.config.json
 cd ~/main.ton.dev/scripts && ./run.sh
+head -n 2 /var/ton-work/node.log
