650e8ea7977146fd44be7d103feee1ae3affff12
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-11-24 01:00:20 +0000

    Update validatorUpdate.sh

--- scripts/validatorUpdate.sh
+++ scripts/validatorUpdate.sh
@@ -8,7 +8,7 @@ git reset --hard origin/master
 cd ~/main.ton.dev/scripts && ./build.sh
 cd ~/main.ton.dev/tonos-cli/target/release/ && ./tonos-cli config --url https://main.ton.dev --wc -1
 cp ~/main.ton.dev/tonos-cli/target/release/tonlabs-cli.conf.json ~/main.ton.dev/scripts/tonlabs-cli.conf.json 
-rm validator_msig.sh && \
+cd ~/main.ton.dev/scripts && rm validator_msig.sh && \
 wget https://raw.githubusercontent.com/tonlabs/net.ton.dev/master/scripts/validator_msig.sh && \
 chmod +x validator_msig.sh
 rm -f /var/ton-work/etc/ton-global.config.json && \
