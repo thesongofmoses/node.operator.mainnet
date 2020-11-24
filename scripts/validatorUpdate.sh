db0358a98e03d882a5bcc0458599c87c3bc121a7
thesongofmoses <67207622+thesongofmoses@users.noreply.github.com>
2020-11-24 00:44:12 +0000

    Update validatorUpdate.sh

--- scripts/validatorUpdate.sh
+++ scripts/validatorUpdate.sh
@@ -13,5 +13,4 @@ cd /var/ton-work/etc/
 wget https://raw.githubusercontent.com/tonlabs/main.ton.dev/master/configs/ton-global.config.json
 cd ~/main.ton.dev/scripts && ./run.sh
 cd ~/main.ton.dev/ton
-git rev-parse HEAD
-head -n 2 /var/ton-work/node.log
+git rev-parse HEAD && head -n 2 /var/ton-work/node.log
